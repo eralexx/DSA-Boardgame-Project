3870394d022c3ddf5fd3d2cbb81838b9cd8b5598
eralexx <dani2000>
2017-10-10 12:56:56 +0200

    Login FrontEnd

--- src/JavaScripts/Login.js
+++ src/JavaScripts/Login.js
@@ -1,5 +1,33 @@
 $(function () {
+    var serverURL="http://localhost:63342/";
+    $( "#login-btn" ).click(function() {
+        var email = $("#email").val();
+        var password = $("#password").val();
+        ExecuteRestQuery(serverURL+"Login/"+email+":"+password);
+    });
 
-    $("#testJquery").append("<h1> JQuery is Working properly on the custom js file</h1>");
+    $( "#register-btn" ).click(function() {
+        var email = $("#email").val();
+        var password = $("#password").val();
+        ExecuteRestQuery(serverURL+"Register/"+email+":"+password);
+    });
+
+    function ExecuteRestQuery(url) {
+        var defer = $.Deferred();
+        $.ajax({
+            url: url,
+            type: "GET",
+            headers: {
+                "Accept": "application/json;odata=verbose",
+            },
+            success: function (data, textStatus, jqXHR) {
+                defer.resolve(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                defer.reject(jqXHR);
+            }
+        });
+        return defer.promise();
+    }
 
 });
